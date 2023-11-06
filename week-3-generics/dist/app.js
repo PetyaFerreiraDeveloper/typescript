d15f9942607b970d2c2417c9901d3259c487477f
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-11-06 21:05:12 +0100

    interfaces demo

---
+++ week-3-generics/dist/app.js
@@ -0,0 +1,10 @@
+const user1 = {
+    firstName: "Pesho",
+    email: "pesho@abv.bg",
+    lastName: "petrov",
+    tellDetails: function () {
+        console.log(`User details: ${this.firstName} ${this.lastName} with email ${this.email}`);
+    },
+};
+user1.tellDetails();
+//# sourceMappingURL=app.js.map
---
+++ week-3-generics/src/app.ts
@@ -0,0 +1,63 @@
+// INTERFACES DEMO
+
+// // Contract
+// interface UserDetails {
+//   firstName: "Tsveti" | "Gosho" | "Pesho"
+//   lastName: string
+//   email?: string
+//   tellDetails?: () => void
+// }
+
+// // interface can be used as a type of a variable
+// const user1: UserDetails = {
+//   firstName: "Pesho",
+//   email: "pesho@abv.bg",
+//   lastName: "petrov",
+//   tellDetails: function () {
+//     console.log(
+//       `User details: ${this.firstName} ${this.lastName} with email ${this.email}`
+//     )
+//   },
+// }
+
+// // user1.tellDetails()
+
+// interface Human {
+//   legsCount: number
+//   skinColor: string
+// }
+
+// interface PersonDetails extends UserDetails, Human {}
+
+// // the interface can be implemented by a class
+// class Person implements PersonDetails {
+//   firstName: "Tsveti" | "Gosho" | "Pesho"
+//   lastName: string
+//   legsCount: number
+//   skinColor: string
+//   fullName: string
+
+//   constructor(
+//     firstName: "Tsveti" | "Gosho" | "Pesho",
+//     lastName: string,
+//     legsCount: number,
+//     skinColor: string
+//   ) {
+//     this.firstName = firstName
+//     this.lastName = lastName
+//     this.legsCount = legsCount
+//     this.skinColor = skinColor
+//     this.fullName = this.firstName + " " + this.lastName
+//   }
+// }
+
+// // can be used as a return type
+// function getUsers(): UserDetails[] {
+//   return [
+//     { firstName: "Pesho", lastName: "Petrov" },
+//     { firstName: "Gosho", lastName: "Ivanov" },
+//   ]
+// }
+
+// TYPES DEMO
+
