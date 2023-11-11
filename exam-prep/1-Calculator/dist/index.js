f62f881a0d6fcf0593c8636685f17b04bdece0d1
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2023-11-11 11:01:01 +0100

    Calculator

---
+++ exam-prep/1-Calculator/dist/index.js
@@ -0,0 +1,23 @@
+function calculator(num1, operator, num2) {
+    if (operator === "+") {
+        console.log((num1 + num2).toFixed(2));
+    }
+    if (operator === "-") {
+        console.log((num1 - num2).toFixed(2));
+    }
+    if (operator === "*") {
+        console.log((num1 * num2).toFixed(2));
+    }
+    if (operator === "/") {
+        if (num2 === 0) {
+            console.log("Divider can not be 0");
+        }
+        else {
+            console.log((num1 / num2).toFixed(2));
+        }
+    }
+}
+calculator(5, "+", 10);
+calculator(25.5, "-", 3);
+calculator(9, "/", 2);
+calculator(7, "*", 5);
